c43258a41baedb5f6dd85f18180d648e88334fd9
amek1004 <you@example.com>
2022-07-15 05:59:40 +0900

    main

---
+++ components/header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div>
+      <h1>헤더메뉴</h1>
+    </div>
+  );
+}
---
+++ pages/404.js
@@ -0,0 +1,7 @@
+export default function PageNotFound() {
+  return (
+    <div>
+      <h1>페이지가 없습니다</h1>
+    </div>
+  );
+}
