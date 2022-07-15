8264f253d0238c3cfbfe53d57e2d24444af0e405
amek1004 <choihantae@me.com>
2022-07-15 15:24:26 +0900

    main

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,14 @@
 export default function PageNotFound() {
   return (
     <div>
-      <h1>페이지가 없습니다</h1>
+      <h1>아무것도 없지 돌아가</h1>
       <style jsx>{`
         h1 {
           background-color: black;
-          color: aliceblue;
-        }
+          color: rgb(255, 0, 0);
+          height: 500px;
+          text-align: center;
+          padding-top: 200px;
       `}</style>
     </div>
   );
